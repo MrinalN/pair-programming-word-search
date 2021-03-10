3c91311b9d2fec7ec5a8384558647f56d7eddc8e
Adele Noronha <adele.c.noronha@gmail.com>
2021-03-10 23:39:09 +0000

    completed pair programming exercise

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  const result = [];
+  for (let x = 0; x < matrix[0].length; x++) {
+    const buffer = [];
+    for (let y = 0; y < matrix.length; y++) {
+      buffer.push(matrix[y][x]);
     }
-}
+    result.push(buffer);
+  }
+  return result;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+
+  let transposedMatrix = transpose(letters);
+  let result = false;
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      result = true;
+    }
+
+  }
+  //console.log(transposedMatrix)
+
+  const transposedMatrix1 = transposedMatrix.map(ls => ls.join(''));
+  for (let x of transposedMatrix1) {
+    console.log(x);
+    if (x.includes(word)) {
+      result = true;
+    }
+
+  }
+
+  return result;
+};
+
+
+module.exports = wordSearch;
